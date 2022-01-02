e5536111478adafe9b43c27c1b92f680f045cce8
Juan Luque <juand1121@gmail.com>
2022-01-02 01:26:01 -0500

    Can now jump to next and prev links.

--- main.ts
+++ main.ts
@@ -41,10 +41,56 @@ export default class MyPlugin extends Plugin {
 			id: 'sample-editor-command',
 			name: 'Sample editor command',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+                let cursorText = editor.getLine(editor.getCursor().line).
+                    slice(editor.getCursor().ch);
+                const regex = /\[\[[^\]]+\]\]/
+                let numLines = editor.lineCount();
+                let currLine = editor.getCursor().line;
+                let i = 0;
+                while (i < numLines) {
+                    let offset : number = i == 0 ? editor.getCursor().ch : 0; 
+                    let text = editor.getLine(currLine).slice(offset);
+                    let match_index = text.search(regex);
+                    if (match_index > -1) {
+                        editor.setCursor(currLine, match_index + offset + 2);
+                        break;
+                    }
+                    i++;
+                    currLine = (currLine + 1) % numLines;
+                }
 			}
 		});
+
+        this.addCommand({
+            id: 'prev-link',
+            name: 'Previous link',
+            editorCallback: (editor: Editor, view: MarkdownView) => {
+                let cursorText = editor.getLine(editor.getCursor().line).
+                    slice(editor.getCursor().ch);
+                const regex = /\[\[[^\]]+\]\](?!.*\[\[.*\]\])/
+                let numLines = editor.lineCount();
+                let currLine = editor.getCursor().line;
+                let i = 0;
+                while (i < numLines) {
+                    let offset : number = i == 0 ? editor.getCursor().ch : -1; 
+                    var text : string;
+                    if (offset < 0) {
+                        text = editor.getLine(currLine);
+                    } else{
+                        text = editor.getLine(currLine).slice(0,offset);
+                    }
+                    let match_index = text.search(regex);
+                    if (match_index > -1) {
+                        //editor.setCursor(currLine, match_index + offset + 2);
+                        editor.setCursor(currLine, match_index + 2);
+                        break;
+                    }
+                    i++;
+                    currLine = (currLine - 1 + numLines) % numLines;
+                }
+            }
+        });
+
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
 			id: 'open-sample-modal-complex',
